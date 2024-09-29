d8496a63975b310207463683aeeb617a851f306f
Am1d1v <dimadiman1adima@gmail.com>
2024-09-29 19:26:53 +0300

    Scene height addaptive configuration

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -4,11 +4,19 @@ import Phaser from "phaser";
 class PlayScene extends Phaser.Scene {
     constructor(){
         super("PlayScene");
+
+        
     };
 
+    // Get game height
+    get gameHeight(){
+        return this.game.config.height as number;
+    }
+
     create(){
+
         // Draw ground
-        this.add.tileSprite(0, 330, 1000, 26, 'ground').setOrigin(0, 1);
+        this.add.tileSprite(0, this.gameHeight, 1000, 26, 'ground').setOrigin(0, 1);
     }
 
 };
