69f6b72fc1794143e9829105792c1e16af235f66
Am1d1v <dimadiman1adima@gmail.com>
2024-09-30 12:26:06 +0300

    Make player Jump

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -8,6 +8,8 @@ class PlayScene extends Phaser.Scene {
         
     };
 
+    player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+
     // Get game height
     get gameHeight(){
         return this.game.config.height as number;
@@ -16,17 +18,27 @@ class PlayScene extends Phaser.Scene {
     create(){  
         this.createSceneEnvironment();
         this.createPlayer();
+        this.registerPlayerControl();
     }
 
     // Draw scene environment
     createSceneEnvironment(){
         // Draw ground
-        return this.add.tileSprite(0, this.gameHeight, 1000, 26, 'ground').setOrigin(0, 1);
+        this.add.tileSprite(0, this.gameHeight, 1000, 26, 'ground').setOrigin(0, 1);
     }
 
     // 
     createPlayer(){
-        return this.physics.add.sprite(0, this.gameHeight, 'player').setOrigin(0, 1);
+        this.player = this.physics.add.sprite(0, this.gameHeight, 'player').setOrigin(0, 1);
+    }
+
+    // Register player inputs
+    registerPlayerControl(){
+        // Jump Up
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        spaceBar.on('down', () => {
+            console.log('Jump');
+        });
     }
 
 };
